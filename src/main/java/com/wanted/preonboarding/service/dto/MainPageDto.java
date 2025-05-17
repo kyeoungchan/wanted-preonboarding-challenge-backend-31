176b4ca60f9fe574c8273f2a29b22e99e76c933e
wkc1002 <wkc1002@naver.com>
2025-05-17 22:16:47 +0900

    feat: MainPage

---
+++ src/main/java/com/wanted/preonboarding/service/dto/MainPageDto.java
@@ -0,0 +1,33 @@
+package com.wanted.preonboarding.service.dto;
+
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class MainPageDto {
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @Builder
+    public static class MainPage {
+        private List<ProductDto.ProductSummary> newProducts;
+        private List<ProductDto.ProductSummary> popularProducts;
+        private List<FeaturedCategory> featuredCategories;
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @Builder
+    public static class FeaturedCategory {
+        private Long id;
+        private String name;
+        private String slug;
+        private String imageUrl;
+        private Integer productCount;
+    }
+}
